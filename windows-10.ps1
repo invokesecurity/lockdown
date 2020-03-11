1dfea3fc47bcffc474038cb59d6303a81e014c16
invokesecurity <daniel.moreland@gmail.com>
2020-03-11 14:09:06 +0100

    added .ps1 extension, adding password complexity

---
+++ windows-10.ps1
@@ -0,0 +1,10 @@
+##enable automatic updates
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
+##set password complexity
+ecedit /export /cfg c:\secpol.cfg
+(GC C:\secpol.cfg) -Replace "PasswordComplexity = 0","PasswordComplexity = 1" | Out-File C:\secpol.cfg
+secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
+Remove-Item C:\secpol.cfg -Force
+##change user password
+Set-ADAccountPassword -Identity $user -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$newPass" -Force)
+
